a4bcdb1549cd226191a44856271c9c8f3c0c9ebb
YuAn <17638141960@163.com>
2020-09-18 18:41:27 +0800

    分类列表

---
+++ js/menus.js
@@ -0,0 +1,374 @@
+const meunsArr = [
+  {
+    title: '狗狗主粮',
+    imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/04/010/0104701_111170.jpg@!50w-c',
+    child:[
+      {
+        title: '进口狗粮',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/04/010/0101719_658143.jpg@!50w-c',
+        children:['小型犬专用','大型犬专用','大型犬幼犬专用','夏日清火']
+      },
+      {
+        title: '国产狗粮',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/04/010/0101433_108893.jpg@!50w-c',
+        children:['怀孕哺乳期犬','贵宾犬粮','大型犬专用','金毛犬粮','中老年犬']
+      },
+      {
+        title: '冻干狗粮',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/04/010/01020010_685362.jpg@!50w-c',
+        children:['鸡肉冻干','牛肉冻干','羊肉冻干','鱼肉冻干']
+      }
+    ]
+  },
+  {
+    title: '狗狗零食',
+    imgurl: 'https://img2.epetbar.com/nowater/2017-08/08/14/fcae773006e5ee3b211866f5ca669821.jpg@!50w-c',
+    child:[
+      {
+        title: '磨牙洁齿',
+        imgurl: 'https://img2.epetbar.com/nowater/2017-05/26/10/2eeb97d6268f555de24ec370fd0de2ad.jpg@!50w-c',
+        children:['饼干','洁齿零食','真骨','皮制咬胶']
+      },
+      {
+        title: '肉制零食',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/04/010/0103901_392937.jpg@!50w-c',
+        children:['烘干零食','风干零食','肉肠','鲜肉零食']
+      },
+      {
+        title: '点心饮料',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/f6c39185cb72a306122dd403c94c3a10.jpg@!50w-c',
+        children:['奶酪零食','布丁','饮料','蛋糕']
+      },
+      {
+        title: '罐头湿粮',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/04/010/0103940_830332.jpg@!50w-c',
+        children:['罐头','餐盒','妙鲜包','幼犬罐头']
+      },
+      {
+        title: '冻干零食',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/04/010/0104209_288501.jpg@!50w-c',
+        children:['颗粒冻干','鹿肉冻干','鸭肉冻干','牛肉冻干']
+      }
+    ]
+  },
+  {
+    title: '狗狗窝垫',
+    imgurl: 'https://img2.epetbar.com/nowater/2018-10/11/23/132c57e01e33026fc6abecbd53cef8ff.jpg@!50w-c',
+    child:[
+      {
+        title: '夏季清凉',
+        imgurl: 'https://img2.epetbar.com/nowater/2017-05/26/10/2eeb97d6268f555de24ec370fd0de2ad.jpg@!50w-c',
+        children:['冰垫','冷暖智能窝','水床垫']
+      },
+      {
+        title: '深睡眠窝',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-10/11/23/9992306702d0257c7605d31cb8dfd904.jpg@!50w-c',
+        children:['不可拆窝窝','部分可拆窝窝','全可拆窝窝']
+      },
+      {
+        title: '柔软睡垫',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-10/11/23/e5c30119eb36ad6b45173dac6b38a27e.jpg@!50w-c',
+        children:['不可拆窝垫','全可拆窝垫']
+      },
+      {
+        title: '睡床沙发',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-10/11/23/85f7ec76b05ebacb8c9972ddfb2ed4e4.jpg@!50w-c',
+        children:['毯子/垫子','家具','棉窝','宠物床']
+      }
+    ]
+  },
+  {
+    title: '狗狗玩具',
+    imgurl: 'https://img2.epetbar.com/nowater/2017-08/08/14/2745a5839c628be3db90159a57ccfa41.jpg@!50w-c',
+    child:[
+      {
+        title: '棉制玩具',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/fcab37ead00d77bcdddf7dabe6c817d7.jpg@!50w-c',
+        children:['俏尾巴','发声棉制玩具','美国进口Worldwise','小型犬专用']
+      },
+      {
+        title: '橡胶玩具',
+        imgurl: 'https://img2.epetbar.com/nowater/2016-07/21/14/874299e54a8cf1488d6b1bead8f8e9bb.jpg@!50w-c',
+        children:['益智玩具','缓食玩具','漏食玩具','巡回玩具','发声橡胶']
+      },
+      {
+        title: '塑料玩具',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/df1ed7120376370a4b5badaae61a0a7e.jpg@!50w-c',
+        children:['小型犬专用','漏食玩具','缓食玩具','益智玩具','巡回玩具']
+      },
+      {
+        title: '其他玩具',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/07/13/1833010_296756.jpg@!50w-c',
+        children:['丝瓜络食用玩具','木质玩具','皮质玩具']
+      }
+    ]
+  },
+  {
+    title: '狗狗清洁',
+    imgurl: 'https://img2.epetbar.com/nowater/2017-10/11/16/9fb820c4c17aec6e045e29d1e0f55ecf.jpg@!50w-c',
+    child:[
+      {
+        title: '狗狗厕所',
+        imgurl: 'https://img2.epetbar.com/nowater/2017-10/11/16/d5ade0444ad0945d671c0a2f914e30fd.jpg@!50w-c',
+        children:['平板厕所','立柱厕所','围栏厕所','口腔湿巾']
+      },
+      {
+        title: '狗狗尿片',
+        imgurl: 'https://img2.epetbar.com/nowater/2017-10/11/16/ba4a24c920d1c9c0a5ee43555f8b900b.jpg@!50w-c',
+        children:['尿片','尿裤']
+      },
+      {
+        title: '狗狗湿巾',
+        imgurl: 'https://img2.epetbar.com/nowater/2017-10/11/16/5a980a2b9737177177c01fc187f52109.jpg@!50w-c',
+        children:['桶装湿巾','日常清洁','五官湿巾','口腔湿巾']
+      },
+      {
+        title: '清洁除臭',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/02/22/163952_616079.jpg@!50w-c',
+        children:['消毒杀菌','除臭消毒','垃圾袋','润爪','排便清洁']
+      }
+    ]
+  },
+  {
+    title: '狗狗保健',
+    imgurl: 'https://img2.epetbar.com/nowater/2017-08/08/14/9059356ce1ad69f7800e7c038ac8c66e.jpg@!50w-c',
+    child:[
+      {
+        title: '强化免疫',
+        imgurl: 'https://img2.epetbar.com/nowater/2017-05/04/16/fd206e6489657294e9067d349ea297f1.jpg@!50w-c',
+        children:['羊奶粉','营养滋补','蛋白粉','孕期保健','营养膏']
+      },
+      {
+        title: '美毛增毛',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2019/12/25/164147_551407.jpg@!50w-c',
+        children:['鱼油','美毛粉','海藻粉','美毛颗粒','美毛膏']
+      },
+      {
+        title: '肠胃调理',
+        imgurl: 'https://img2.epetbar.com/nowater/2016-08/20/14/8095e54e9414a06705acb7471eac7629.jpg@!50w-c',
+        children:['益生菌','整肠粉/片','消食片','健脾膏/片']
+      },
+      {
+        title: '补钙强骨',
+        imgurl: 'https://img2.epetbar.com/nowater/2016-08/20/14/19c089474d2c89f98435b088fb30b404.jpg@!50w-c',
+        children:['钙片','液体钙','钙粉','钙膏']
+      },
+      {
+        title: '微量元素',
+        imgurl: 'https://img2.epetbar.com/nowater/2016-08/20/14/55aff065670558cb4ff66423f9ebc09e.jpg@!50w-c',
+        children:['微量元素片','微量元素粉','维生素','B族维生素']
+      },
+      {
+        title: '关节呵护',
+        imgurl: 'https://img2.epetbar.com/nowater/2016-08/20/14/cc2ec24f1b68fe6f648aada3bc707959.jpg@!50w-c',
+        children:['关节片','关节颗粒','关节粉']
+      }
+    ]
+  },
+  {
+    title: '狗狗护理',
+    imgurl: 'https://img2.epetbar.com/nowater/2017-08/08/14/ab4c5bbee6ea1f9d4b8064d3523df7c6.jpg@!50w-c',
+    child:[
+      {
+        title: '体外驱虫',
+        imgurl: 'https://img2.epetbar.com/nowater/2016-07/23/14/c52111e9c4150f10ef7c5f9a1806fc93.jpg@!50w-c',
+        children:['跳蚤虱子','防蚊虫','螨虫','环境驱虫']
+      },
+      {
+        title: '体内驱虫',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/06/03/113202_655754.jpg@!50w-c',
+        children:['心丝虫','弓形虫','肠道寄生虫']
+      },
+      {
+        title: '皮肤护理',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/04/08/175525_17953.jpg@!50w-c',
+        children:['皮肤修复','真菌','脓皮症','皮炎']
+      },
+      {
+        title: '耳部护理',
+        imgurl: 'https://img2.epetbar.com/common/upload/commonfile/2020/04/08/175540_733793.jpg@!50w-c',
+        children:['日常清洁','耳部炎症','洗耳水','康牧耳康']
+      },
+      {
+        title: '眼部护理',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-01/25/14/4b27726f0f1a44b34cfe9a32965ceafb.jpg@!50w-c',
+        children:['牛磺酸','洗眼液','泪痕粉/水','眼膏']
+      },
+      {
+        title: '口鼻护理',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-01/25/15/137ab5ae324905b93aa37503b053ad19.jpg@!50w-c',
+        children:['牙刷牙膏','口腔喷/啫喱','鼻涕膏','牙结石','漱口水']
+      },
+      {
+        title: '家庭常备',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/7d009bab6bcdaeb941f18bd510aa9d56.jpg@!50w-c',
+        children:['伊丽莎白圈','感冒药','止痛喷','止泻药']
+      }
+    ]
+  },
+  {
+    title: '狗狗生活',
+    imgurl: 'https://img2.epetbar.com/nowater/2017-08/08/14/e8e90fcf991a2932a782db5b8cfa18fc.jpg@!50w-c',
+    child:[
+      {
+        title: '狗狗餐具',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/b686acdc3b8a6e0e635fac80f1311214.jpg@!50w-c',
+        children:['狗碗','饮水机','组合餐具','储粮用品']
+      },
+      {
+        title: '狗狗外出',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/643e72faf7dd1659ed632b2f40e59e75.jpg@!50w-c',
+        children:['太空包','移动推车','外出背包','航空托运']
+      },
+      {
+        title: '狗狗住所',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/1078c799ae6f02bf4255fd0919b813e9.jpg@!50w-c',
+        children:['毯子/垫子','围栏','方管笼','狗家具']
+      },
+      {
+        title: '户外运动',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-03/29/10/f8cc4d8fbf1d940f99531d73d770876d.jpg@!50w-c',
+        children:['外出餐具','防护衣','登上背包','救生衣']
+      },
+      {
+        title: '狗狗训练',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/590d323712426cc4383209983d4599eb.jpg@!50w-c',
+        children:['嘴套','禁区喷','爱心拍','排便诱导']
+      },
+      {
+        title: '主人专享',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-03/29/10/8cd0158bc6fef2009533679c02319219.jpg@!50w-c',
+        children:['吾皇万岁','生活用品','手机配件','车贴']
+      }
+    ]
+  },
+  {
+    title: '狗狗牵引',
+    imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/38be057278d61f458ccadc2b692f8968.jpg@!50w-c',
+    child:[
+      {
+        title: '伸缩牵引',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/3cc32faf42f48f93b2534bef920623ee.jpg@!50w-c',
+        children:['三大专利 伸缩牵引','小型犬专用','中型犬专用','大型犬专用']
+      },
+      {
+        title: '日常颈圈',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/934bbc53f04789a194bddf4007af984c.jpg@!50w-c',
+        children:['LED项圈','尼龙项圈','牛皮项圈','除骚圈']
+      },
+      {
+        title: '日常拉带',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/7b322fbe812723d414b51e703052c7d8.jpg@!50w-c',
+        children:['尼龙拉带','棉质拉带','牛皮拉带']
+      },
+      {
+        title: '拉颈套装',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/363a030ee7a5c9a4c46870ba74ef5d96.jpg@!50w-c',
+        children:['小型犬','中型犬','尼龙拉颈套装']
+      },
+      {
+        title: '拉胸套装',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/ea817e6228e36533399ea6cbac068ee5.jpg@!50w-c',
+        children:['经典尼龙胸背带','金属','日常胸带','日常拉胸套装']
+      },
+      {
+        title: 'P链/蛇链',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/6e7c85be18f569be27d58d1c50f06b85.jpg@!50w-c',
+        children:['合金','尼龙']
+      }
+    ]
+  },
+  {
+    title: '出游洗澡',
+    imgurl: 'https://img2.epetbar.com/nowater/2018-07/06/21/f3017e41869a85402401eeb99498aeb2.jpg@!50w-c',
+    child:[
+      {
+        title: '专用香波',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/ec484631ffe713596eebce2797a8b21f.jpg@!50w-c',
+        children:['双层被毛','卷毛专用','红棕色','金色专用']
+      },
+      {
+        title: '日常洗护',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-07/18/16/57b2254d23adfd82a25a2f65c6adc789.jpg@!50w-c',
+        children:['滋养柔顺','除臭添香','低敏舒缓','蓬松塑型','超强去污']
+      },
+      {
+        title: '免洗干洗',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/e5197ce68f598bfaa20cda64460fb26b.jpg@!50w-c',
+        children:['干洗泡沫','干洗水','干洗粉','干洗喷剂','保湿喷']
+      },
+      {
+        title: '洗澡工具',
+        imgurl: 'https://img2.epetbar.com/nowater/2016-02/18/16/365564368d9b40cca9833adafb6ed63f.jpg@!50w-c',
+        children:['吸水毛巾','洗澡刷','吹水机','吹风机']
+      }
+    ]
+  },
+  {
+    title: '狗狗服饰',
+    imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/b6bfa4ad0717d26d2ad40093a21b3806.jpg@!50w-c',
+    child:[
+      {
+        title: '御寒棉服',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-10/12/00/556a307689a1bda2299be27956015cd4.jpg@!50w-c',
+        children:['外套','马甲','休闲风','小清新']
+      },
+      {
+        title: '时尚卫衣',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-10/12/00/332cb407ae3df288233aa8e46507ccde.jpg@!50w-c',
+        children:['休闲风','运动装','小清新','甜美风']
+      },
+      {
+        title: '潮流四脚',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-10/12/00/51e8e71ae5afe739039bf46ff5057c8e.jpg@!50w-c',
+        children:['休闲风','甜美风','英伦风','小清新']
+      },
+      {
+        title: '针织毛衣',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-10/12/00/b537f6235f3cab502e8703699150ede4.jpg@!50w-c',
+        children:['小型犬','中型犬','小情新','休闲风']
+      },
+      {
+        title: '甜美淑女',
+        imgurl: 'https://img2.epetbar.com/nowater/2018-10/12/00/0071af1e77ccd8f4d7872790aa92479b.jpg@!50w-c',
+        children:['裙装','外套','衬衫','秋冬']
+      },
+      {
+        title: '四季雨衣',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/7feeef10bab3d2e8df8a9149e0cb7c70.jpg@!50w-c',
+        children:['中型犬','大狗专区','小型犬专区']
+      },
+      {
+        title: '精品配饰',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/1d55d100e176bd571e4b0fd85604a14d.jpg@!50w-c',
+        children:['棒球帽','项链','头花','领结','太阳帽']
+      },
+      {
+        title: '断码反季',
+        imgurl: 'https://img2.epetbar.com/nowater/2017-10/10/16/d55b1ff4802aa8b6eb402c6292022084.jpg@!50w-c',
+        children:['棉衣','变身装','四脚装','家居服','背带裤']
+      }
+    ]
+  },
+  {
+    title: '狗狗美容',
+    imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/1bba87b72fe176bb1241ab68465f7e25.jpg@!50w-c',
+    child:[
+      {
+        title: '梳剪工具',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/9e337e765abe013fd2b4742076dfbac8.jpg@!50w-c',
+        children:['针梳','脱毛梳','排梳','开结梳','电推剪']
+      },
+      {
+        title: '护理工具',
+        imgurl: 'https://img2.epetbar.com/nowater/cates/2014-03/24/1e78dcc3b144aa0e5fdfc9fcea1b1be3.jpg@!50w-c',
+        children:['指甲剪','磨甲器','拔耳粉','指甲锉']
+      },
+      {
+        title: '美容配套',
+        imgurl: 'https://img2.epetbar.com/nowater/2016-02/18/16/e1b0d47ed59204385e33be0bf078513e.jpg@!50w-c',
+        children:['浴盐','香水','精油','染色剂','造型用品']
+      }
+    ]
+  }
+]
